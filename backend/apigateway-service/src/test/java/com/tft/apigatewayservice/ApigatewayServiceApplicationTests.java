7a87596a1b4078890ec0e3269203cc9e30479ce6
김대홍 <rlaeoghd1885@naver.com>
2023-09-08 14:58:10 +0900

    [BE] Init : MSA - 초기 프로젝트 설정

---
+++ backend/apigateway-service/src/test/java/com/tft/apigatewayservice/ApigatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tft.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
