06308688617225b5d1604698f6b3f61f011d1e0e
전윤철 <tlfldntm4444@naver.com>
2023-09-15 12:02:21 +0900

    [BE] Add : gradle 기본 설정 파일 추가

---
+++ backend/payment-service/src/test/java/com/tft/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tft.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
