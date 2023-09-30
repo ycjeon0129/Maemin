18d91984d76ba1785ae49cb52e519ccde0803d24
전윤철 <tlfldntm4444@naver.com>
2023-09-30 14:14:38 +0900

    [BE] Fix : payment - 결제 오류 수정 (PayFeignClient url 삭제)

--- backend/payment-service/src/main/java/com/tft/paymentservice/common/feign/PayFeignClient.java
+++ backend/payment-service/src/main/java/com/tft/paymentservice/common/feign/PayFeignClient.java
@@ -7,7 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "pay-service", url = "${custom.url}"+"/pay-service")
+//@FeignClient(name = "pay-service", url = "${custom.url}"+"/pay-service")
+@FeignClient(name = "pay-service")
 public interface PayFeignClient {
 
     //    @PostMapping("/pay/authentication")
