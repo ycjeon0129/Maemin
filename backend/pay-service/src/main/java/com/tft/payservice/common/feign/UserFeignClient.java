19e9f8ab4b074c75d3aa5ca1400dd0d7806d3b1f
전윤철 <tlfldntm4444@naver.com>
2023-10-04 14:53:07 +0900

    [BE] Fix : pay - UserFeignClient 파라미터 수정

--- backend/pay-service/src/main/java/com/tft/payservice/common/feign/UserFeignClient.java
+++ backend/pay-service/src/main/java/com/tft/payservice/common/feign/UserFeignClient.java
@@ -2,12 +2,12 @@ package com.tft.payservice.common.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "user-service", url = "${custom.url}"+"/user-service")
 public interface UserFeignClient {
 
-    @PatchMapping("/users/pay")
-    int joinPay(@RequestBody boolean status);
+    @PatchMapping("/users/pay/{status}")
+    int joinPay(@PathVariable boolean status);
 
 }
