2562fc3e2f47e3d3bfb1ed3edcbcbc4790d679be
전윤철 <tlfldntm4444@naver.com>
2023-10-04 14:21:40 +0900

    [BE] Fix : pay - EnableFeignClients annotation 추가

--- backend/pay-service/src/main/java/com/tft/payservice/PayServiceApplication.java
+++ backend/pay-service/src/main/java/com/tft/payservice/PayServiceApplication.java
@@ -3,6 +3,7 @@ package com.tft.payservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -11,7 +12,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
-//@EnableFeignClients
+@EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
 public class PayServiceApplication implements WebApplicationInitializer {
