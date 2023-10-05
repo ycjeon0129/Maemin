3f6887ea241b46451344b28d78fd9792aa640765
김대홍 <rlaeoghd1885@naver.com>
2023-10-05 14:53:44 +0900

    [BE] Refactor : sse 저장방식 변경

--- backend/cart-service/src/main/java/com/tft/cartservice/sse/service/SseEmitters.java
+++ backend/cart-service/src/main/java/com/tft/cartservice/sse/service/SseEmitters.java
@@ -6,10 +6,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,22 +15,22 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class SseEmitters {
 
     // Map을 사용하여 storeId와 SSE 연결을 매핑합니다.
-    private final Map<String, List<SseEmitter>> storeEmitters = new ConcurrentHashMap<>();
+    private final Map<String, SseEmitter> storeEmitters = new ConcurrentHashMap<>();
 
-//    @Transactional
+    //    @Transactional
     public SseEmitter add(String storeId, SseEmitter emitter) {
         // 가게별로 SSE 연결 리스트를 가져옵니다.
-        List<SseEmitter> emitters = storeEmitters.computeIfAbsent(storeId, key -> new CopyOnWriteArrayList<>());
-
-        // SSE 연결을 리스트에 추가합니다.
-        emitters.add(emitter);
+//        List<SseEmitter> emitters = storeEmitters.computeIfAbsent(storeId, key -> new CopyOnWriteArrayList<>());
+//
+//        // SSE 연결을 리스트에 추가합니다.
+//        emitters.add(emitter);
+        storeEmitters.put(storeId, emitter);
 
         log.info("new emitter added for store {}: {}", storeId, emitter);
-        log.info("emitter list size for store {}: {}", storeId, emitters.size());
 
         emitter.onCompletion(() -> {
             // SSE 연결이 완료될 때 해당 연결을 리스트에서 삭제합니다.
-            emitters.remove(emitter);
+            storeEmitters.remove(storeId);
             log.info("emitter removed for store {}: {}", storeId, emitter);
         });
 
@@ -47,20 +45,18 @@ public class SseEmitters {
 
     public void sendOrderAlert(String storeId, String message) {
         // 특정 가게에 대한 주문 알람을 보냅니다.
-        List<SseEmitter> emitters = storeEmitters.get(storeId);
-        log.info("send 준비");
-        log.info("보내기 전 emitters 길이 체크 : {}", String.valueOf(emitters.size()));
-        if (emitters != null) {
-            emitters.forEach(emitter -> {
-                try {
-                    log.info(String.valueOf(emitter));
-                    emitter.send(SseEmitter.event()
-                            .name("order")
-                            .data(message));
-                } catch (IOException e) {
-                    log.error("Error sending order alert to store {}: {}", storeId, e.getMessage());
-                }
-            });
+        SseEmitter emitter = storeEmitters.get(storeId);
+        log.info("send 준비 : {}", emitter);
+        if (emitter != null) {
+            try {
+                log.info(String.valueOf(emitter));
+                emitter.send(SseEmitter.event()
+                        .name("order")
+                        .data(message));
+            } catch (IOException e) {
+                log.error("Error sending order alert to store {}: {}", storeId, e.getMessage());
+                storeEmitters.remove(storeId);
+            }
         }
     }
 }
