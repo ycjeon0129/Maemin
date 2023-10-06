3e3f478452ce03efe1d2a356c0c736b7fe2f2a04
전윤철 <tlfldntm4444@naver.com>
2023-10-06 11:25:28 +0900

    [BE] Fix : store - Feign Request Header 에 토큰 추가

--- backend/store-service/src/main/java/com/tft/storeservice/config/FeignConfig.java
+++ backend/store-service/src/main/java/com/tft/storeservice/config/FeignConfig.java
@@ -1,10 +1,12 @@
 package com.tft.storeservice.config;
 
+import com.tft.storeservice.common.util.RequestUtil;
 import feign.Client;
 import feign.Logger;
 import feign.RequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class FeignConfig {
@@ -24,6 +26,8 @@ public class FeignConfig {
         return requestTemplate -> {
             requestTemplate.header("Content-Type", "application/json");
             requestTemplate.header("Accept", "application/json");
+            requestTemplate.header(HttpHeaders.AUTHORIZATION, RequestUtil.getToken());
+            requestTemplate.header("user-id", RequestUtil.getUserId().toString());
         };
     }
 
